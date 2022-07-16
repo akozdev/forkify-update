2183cd65dc6ff0b29aca5c68d4ca518b81c9055f
akozdev <akoz@hey.com>
2022-07-16 19:29:56 +0200

    init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9a2453db-21e9-439b-9764-f62690c095dc';
+export const MODAL_CLOSE_SEC = 2.5;
